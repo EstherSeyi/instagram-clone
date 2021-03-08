b8e5e43bc25bec06989000e7bb6d671f515392d6
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-08 14:07:52 +0100

    Exports field value from firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,3 +7,10 @@ const config = {
   appId: "1:353571672102:web:599424b144ffc4cbb456e7",
   measurementId: "G-T5M3BZYGRD",
 };
+
+const firebase = window.firebase.initializeApp(config);
+
+// Allow us access to array methods.
+const { FieldValue } = window.firebase.firestore;
+
+export { firebase, FieldValue };
