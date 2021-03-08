b00009f7761b5e9ab187416566427da6faababa8
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-08 14:05:00 +0100

    adds firebase config

---
+++ src/lib/firebase.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: "AIzaSyDxzNhSQoulR2qfHvc9Yncvyqjg00tBAys",
+  authDomain: "instagram-cl-a0c05.firebaseapp.com",
+  projectId: "instagram-cl-a0c05",
+  storageBucket: "instagram-cl-a0c05.appspot.com",
+  messagingSenderId: "353571672102",
+  appId: "1:353571672102:web:599424b144ffc4cbb456e7",
+  measurementId: "G-T5M3BZYGRD",
+};
