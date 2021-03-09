aa3312b1becc281739ad9f0976b989a9ceef92cb
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-09 18:52:55 +0100

    modifies firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,16 +1,23 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+
+// import { seedDatabase } from "../seed";
+
 const config = {
-  apiKey: "AIzaSyDxzNhSQoulR2qfHvc9Yncvyqjg00tBAys",
-  authDomain: "instagram-cl-a0c05.firebaseapp.com",
-  projectId: "instagram-cl-a0c05",
-  storageBucket: "instagram-cl-a0c05.appspot.com",
-  messagingSenderId: "353571672102",
-  appId: "1:353571672102:web:599424b144ffc4cbb456e7",
-  measurementId: "G-T5M3BZYGRD",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
-const firebase = window.firebase.initializeApp(config);
+const firebaseApp = firebase.initializeApp(config);
+const { FieldValue } = firebaseApp.firestore();
 
-// Allow us access to array methods.
-const { FieldValue } = window.firebase.firestore;
-
-export { firebase, FieldValue };
+// seedDatabase(firebase);
+export { firebaseApp as firebase, FieldValue };
