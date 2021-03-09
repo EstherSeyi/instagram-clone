2c3602a9a4c8ca751345a22ce76e2f77baf4177e
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-09 19:55:08 +0100

    modifies firebase context

--- src/context/firebase.js
+++ src/context/firebase.js
@@ -2,19 +2,19 @@ import { createContext } from "react";
 
 import { firebase, FieldValue } from "../lib/firebase";
 
-const { Provider, Consumer } = createContext(null);
+const FirebaseContext = createContext(null);
 
 const FirebaseProvider = ({ children }) => {
   return (
-    <Provider
+    <FirebaseContext.Provider
       value={{
         firebase,
         FieldValue,
       }}
     >
       {children}
-    </Provider>
+    </FirebaseContext.Provider>
   );
 };
 
-export { FirebaseProvider, Consumer as FirebaseConsumer };
+export { FirebaseProvider, FirebaseContext };
