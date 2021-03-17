3d01b68d7d3a2c77d3fe6acbe0d22ddc95848ca4
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-17 08:38:08 +0100

    Adds relevant icons

---
+++ src/components/icons/home.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <svg
+      className="w-8 mr-6 text-black-light cursor-pointer"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+    </svg>
+  );
+};
+
+export default Home;
