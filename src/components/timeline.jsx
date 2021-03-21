1261c5435ae6069d9ab2932778d63d62415ff292
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-21 09:57:30 +0100

    adds timeline subcomponents

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -1,3 +1,28 @@
+import Skeleton from "react-loading-skeleton";
+
+import Post from "./post";
+
+import useFollowedUsersPhotos from "../hooks/useFollowedUsersPhotos";
+import React from "react";
+
 export default function Timeline() {
-  return <p>I am the timeline</p>;
+  const { photos } = useFollowedUsersPhotos();
+
+  return (
+    <section className="max-w-600 col-span-2">
+      {!photos ? (
+        <Skeleton count={4} width={600} height={500} className="mb-5" />
+      ) : photos?.length ? (
+        photos.map((photo) => {
+          return (
+            <React.Fragment key={photo.docId}>
+              <Post content={photo} />
+            </React.Fragment>
+          );
+        })
+      ) : (
+        <p className="text-center text-2xl">Follow people to se photos</p>
+      )}
+    </section>
+  );
 }
