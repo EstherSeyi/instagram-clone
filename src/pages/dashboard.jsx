98565d969300f7d054cd0dd8baff01799ae01351
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-21 10:02:06 +0100

    modifies dashboard background color style

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -10,7 +10,7 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <div className="bg-gray-100">
+    <div className="bg-alabaster">
       <Header />
       <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
         <Timeline />
