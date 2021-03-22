02f42c5fcf7984db2ba206e9dc72e0c9cc970d00
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-22 12:16:40 +0100

    Adds side bar for suggestions

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,3 +1,19 @@
+import Skeleton from "react-loading-skeleton";
+
+import useUser from "../../hooks/useUser";
+
+import User from "./user";
+import Suggestions from "./suggestions";
+
 export default function Sidebar() {
-  return <p>I am the sidebar</p>;
+  const {
+    user: { docId, userId, following, username, fullName } = {},
+  } = useUser();
+
+  return (
+    <aside className="hidden md:block">
+      <User fullName={fullName} username={username} />
+      <Suggestions userId={userId} />
+    </aside>
+  );
 }
