8e2a9de84498679b925268e933aa8fa0aa27009f
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-23 21:42:00 +0100

    modifies profile page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,3 +1,34 @@
+import { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+
+import Header from "../components/header";
+
+import UserProfile from "../components/profile";
+import { getUserByUsername } from "../services/firebase";
+import * as ROUTES from "../constants/routes";
+
 export default function Profile() {
-  return <p>Hello from Profile</p>;
+  const { username } = useParams();
+  const [userExists, setUserExists] = useState(undefined);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function checkUserExistsToLoadProfile() {
+      const doesUserExist = await getUserByUsername(username);
+      if (!doesUserExist) {
+        history.push(ROUTES.NOT_FOUND);
+      } else {
+        setUserExists(true);
+      }
+    }
+
+    checkUserExistsToLoadProfile();
+  }, [username, history]);
+
+  return userExists ? (
+    <>
+      <Header />
+      <UserProfile username={username} />
+    </>
+  ) : null;
 }
