000574d8d5e63ed012dc929609f2d614042a8ed8
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-24 13:36:06 +0100

    adds protected routes component

---
+++ src/helpers/protectedRoutes.jsx
@@ -0,0 +1,22 @@
+import { Redirect, useLocation, Route } from "react-router-dom";
+
+import { useAuth } from "../context/user";
+
+const ProtectedRoute = ({ children }) => {
+  const { auth } = useAuth();
+
+  const location = useLocation();
+
+  return !auth.user ? (
+    <Redirect
+      to={{
+        pathname: "/login",
+        state: { url: location.pathname },
+      }}
+    />
+  ) : (
+    <>{children}</>
+  );
+};
+
+export default ProtectedRoute;
