b1b86a4fcdd116efe1602f4e5e4f338783d9eca9
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-24 13:36:40 +0100

    changes route to jsx

---
+++ src/routes/index.jsx
@@ -0,0 +1,36 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import * as ROUTES from "../constants/routes";
+
+import Loading from "../components/loading";
+
+import PrivateRoutes from "../helpers/protectedRoutes";
+
+const Dashboard = lazy(() => import("../pages/dashboard"));
+const Login = lazy(() => import("../pages/login"));
+const SignUp = lazy(() => import("../pages/signup"));
+const Profile = lazy(() => import("../pages/profile"));
+const NotFound = lazy(() => import("../pages/not-found"));
+
+const Routes = () => {
+  return (
+    <Router>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.PROFILE} component={Profile} />
+
+          <PrivateRoutes path={ROUTES.DASHBOARD} exact={true}>
+            <Dashboard />
+          </PrivateRoutes>
+
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default Routes;
