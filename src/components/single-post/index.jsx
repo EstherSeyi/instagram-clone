d16e2a29c4c5427f08fab44c44f7ec486dcc2580
EstherSeyi <sheyiogundijo@gmail.com>
2021-03-29 17:43:18 +0100

    modifies single post component

--- src/components/single-post/index.jsx
+++ src/components/single-post/index.jsx
@@ -1,8 +1,43 @@
-const SinglePost = () => {
+import { useState, useRef } from "react";
+
+import Header from "./header";
+import Comments from "./comments";
+import Actions from "./actions";
+import AddComment from "./add-comment";
+
+// import useFollowedUsersPhotos from "../../hooks/useFollowedUsersPhotos";
+
+const SinglePost = ({ comments, poster, actions }) => {
+  // const { photos } = useFollowedUsersPhotos();
+  const [showForm, setShowForm] = useState(false);
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
+
+  console.log(actions);
+
   return (
-    <>
-      <p>I am a sample modal</p>
-    </>
+    <div className="bg-white flex flex-col md:flex-row">
+      <Header small poster={poster} />
+      <div className="flex-60">
+        <img
+          className="w-2/3 mx-auto md:m-0 md:w-full h-full"
+          src={`${process.env.PUBLIC_URL}/assets/images/avatars/karl.jpg`}
+          alt="post"
+        />
+      </div>
+      <div className="flex-40 py-4 relative ">
+        <Header poster={poster} />
+        <Comments comments={comments} poster={poster} />
+        <div className="bg-white md:border-t md:border-mecury2 md:absolute right-0 left-0 bottom-0">
+          <Actions
+            setShowForm={setShowForm}
+            handleFocus={handleFocus}
+            actions={actions}
+          />
+          <AddComment showForm={showForm} commentInput={commentInput} />
+        </div>
+      </div>
+    </div>
   );
 };
 
