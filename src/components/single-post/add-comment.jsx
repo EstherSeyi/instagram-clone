a9f32fca774a5810a9c0309be098a1334f6396aa
EstherSeyi <sheyiogundijo@gmail.com>
2021-04-04 20:52:23 +0100

    adds add-comment functionality to single post

--- src/components/single-post/add-comment.jsx
+++ src/components/single-post/add-comment.jsx
@@ -1,15 +1,60 @@
 import { useState } from "react";
 
-const AddComment = ({ commentInput, showForm }) => {
+import { useFirebase } from "../../context/firebase";
+import useUser from "../../hooks/useUser";
+
+const AddComment = ({ commentInput, showForm, setComments, docId }) => {
   const [values, setValues] = useState({
     comment: "",
     loading: false,
   });
+  const { firebase, FieldValue } = useFirebase();
+  const { user } = useUser();
 
   const isValid = values?.comment && values.comment?.length > 3;
 
-  const handleSubmit = () => {};
-  const handleChange = () => {};
+  const handleChange = ({ target }) => {
+    setValues((prevState) => ({
+      ...prevState,
+      comment: target.value,
+    }));
+    console.log(values);
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      setValues((prevState) => ({
+        ...prevState,
+        loading: true,
+      }));
+      await firebase
+        .firestore()
+        .collection("photos")
+        .doc(docId)
+        .update({
+          comments: FieldValue.arrayUnion({
+            displayName: user.username,
+            comment: values.comment,
+          }),
+        });
+      setComments((prevState) => [
+        { displayName: user.username, comment: values.comment },
+        ...prevState,
+      ]);
+      setValues((prevState) => ({
+        ...prevState,
+        comment: "",
+        loading: false,
+      }));
+    } catch (error) {
+      console.log(error.message);
+      setValues((prevState) => ({
+        ...prevState,
+        comment: "",
+        loading: false,
+      }));
+    }
+  };
 
   return (
     <div
@@ -28,7 +73,7 @@ const AddComment = ({ commentInput, showForm }) => {
           autoComplete="off"
           className=" blocktext-14 text-gray w-10/12 mr-3 py-3 px-4 focus:outline-none"
           type="text"
-          name="add-comment"
+          name="comment"
           placeholder="Add a comment..."
           value={values.comment}
           onChange={handleChange}
