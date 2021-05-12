c882de581dabcd3658057b67d12cf225f877cde4
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-05-12 16:56:29 +0100

    modifes view posts component

--- src/components/post/comments.jsx
+++ src/components/post/comments.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import { formatDistance } from "date-fns";
 
@@ -7,56 +6,35 @@ import SinglePost from "../single-post";
 
 import { useModal } from "../../context/modal";
 
-export default function Comments({
-  docId,
-  comments: allComments,
-  posted,
-  commentInput,
-  poster,
-  likeDetails,
-}) {
+export default function Comments({ content, commentInput }) {
   const { open } = useModal();
-  const [comments, setComments] = useState(allComments);
 
   return (
     <>
       <div className="pl-4 pt-1 pb-4 text-14">
-        {comments.length >= 3 && (
+        {content.comments.length >= 3 && (
           <Link
             to="#"
-            onClick={() =>
-              open(
-                <SinglePost
-                  poster={poster}
-                  comments={comments}
-                  likeDetails={{ ...likeDetails, docId }}
-                />
-              )
-            }
+            onClick={() => open(<SinglePost content={content} />)}
             className="text-quickSilver2 font-bold"
           >
-            View all {comments.length} comments
+            View all {content.comments.length} comments
           </Link>
         )}
 
-        {comments.slice(0, 2).map((item) => (
-          <p key={`${item.comment}-${item.displayName}`}>
-            <Link to={`/p/${item.displayName}`} className="font-bold mr-1">
-              {item.displayName}
+        {content.comments.slice(0, 2).map((item) => (
+          <p key={`${item.comment}-${item.user.username}`}>
+            <Link to={`/p/${item.user.username}`} className="font-bold mr-1">
+              {item.user.username}
             </Link>
             <span>{item.comment}</span>
           </p>
         ))}
         <p className="text-quickSilver2 uppercase mt-2 text-10">
-          {formatDistance(posted, new Date())} ago
+          {formatDistance(new Date(content.createdAt), new Date())} ago
         </p>
       </div>
-      <AddComment
-        docId={docId}
-        comments={comments}
-        setComments={setComments}
-        commentInput={commentInput}
-      />
+      <AddComment photoId={content._id} commentInput={commentInput} />
     </>
   );
 }
