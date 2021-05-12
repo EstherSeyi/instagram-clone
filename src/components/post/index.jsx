6b4b808340a39b282a8655dd910f334f23de5c53
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-05-12 16:57:21 +0100

    modifies post component

--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -5,35 +5,21 @@ import Image from "./image";
 import Actions from "./actions";
 import Footer from "./footer";
 import Comments from "./comments";
+
 const Post = ({ content }) => {
   const commentInput = useRef(null);
   const handleFocus = () => commentInput.current.focus();
+
   return (
     <div className="rounded col-span-4 border border-mecury mb-16 bg-white">
-      <Header username={content.username} />
+      <Header
+        username={content.user.username}
+        avatarSrc={content.user.avatarSrc}
+      />
       <Image src={content.imageSrc} caption={content.caption} />
-      <Actions
-        docId={content.docId}
-        totalLikes={content.likes.length}
-        likedPhoto={content.userLikedPhoto}
-        handleFocus={handleFocus}
-      />
-      <Footer username={content.username} caption={content.caption} />
-      <Comments
-        docId={content.docId}
-        comments={content.comments}
-        posted={content.dateCreated}
-        commentInput={commentInput}
-        poster={{
-          id: content.userId,
-          username: content.username,
-          image: content.imageSrc,
-        }}
-        likeDetails={{
-          totalLikes: content.likes.length,
-          likedPhoto: content.userLikedPhoto,
-        }}
-      />
+      <Actions handleFocus={handleFocus} content={content} />
+      <Footer username={content.user.username} caption={content.caption} />
+      <Comments content={content} commentInput={commentInput} />
     </div>
   );
 };
