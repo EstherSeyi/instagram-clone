bed3cca7ee20da185cbcb512f60a0a7f7351655b
Esther Oluwaseyi <40301524+EstherSeyi@users.noreply.github.com>
2021-05-12 17:21:38 +0100

    modifies dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,20 +1,37 @@
 import { useEffect } from "react";
+import { useAppQuery } from "../hooks/use-query-helpers";
 
 import Header from "../components/header";
 import Timeline from "../components/timeline";
-import Sidebar from "../components/sidebar/";
+// import Sidebar from "../components/sidebar/";
+
+import { useAuth } from "../context/Auth";
 
 export default function Dashboard() {
+  const { state, addUser } = useAuth();
   useEffect(() => {
     document.title = "Instagram";
   }, []);
+
+  useAppQuery(
+    `user-data_${state?.user?.id}`,
+    {
+      url: `v1/user/${state?.user?.id}`,
+    },
+    {
+      enabled: state.isLoggedIn,
+      onSuccess: (data) => {
+        addUser({ ...data?.payload, id: data?.payload?._id });
+      },
+    }
+  );
 
   return (
     <div className="bg-alabaster">
       <Header />
       <div className="md:grid grid-cols-3 gap-4 justify-between mx-auto  w-11/12 max-w-screen-lg">
-        <Timeline />
-        <Sidebar />
+        <Timeline userId={state?.user?.id} />
+        {/* <Sidebar /> */}
       </div>
     </div>
   );
